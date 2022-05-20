7c9fa0b7f0fb3ebc2412b7f6c71e29f78cfd2956
Carlos <carlos.emmanuel.236@gmail.com>
2022-05-20 20:11:13 -0300

    Adicionando testes do model subject

--- test/models/subject_test.rb
+++ test/models/subject_test.rb
@@ -1,7 +1,42 @@
 require "test_helper"
 
 class SubjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Deve salvar uma matéria cadastrada corretamente" do
+    user = User.new nome: "Usuario Teste",
+                    email: "usuarioteste@gmail.com",
+                    password: "123456",
+                    password_confirmation: "123456"
+    assert user.save
+
+    classroom = Classroom.new school_year: "8",
+                              letter: "A",
+                              year: "2022",
+                              user_id: user.id
+    assert classroom.save
+
+    subject = Subject.new name: "Matemática",
+                          year: "2022",
+                          classroom_id: classroom.id
+    assert subject.save
+  end
+
+  test "Não deve salvar uma matéria sem ano" do
+    user = User.new nome: "Usuario Teste",
+                    email: "usuarioteste@gmail.com",
+                    password: "123456",
+                    password_confirmation: "123456"
+    assert user.save
+
+    classroom = Classroom.new school_year: "8",
+                              letter: "A",
+                              year: "2022",
+                              user_id: user.id
+    assert classroom.save
+
+    subject = Subject.new name: "Matemática",
+                          year: nil,
+                          classroom_id: classroom.id
+    assert_not subject.save
+  end
 end
