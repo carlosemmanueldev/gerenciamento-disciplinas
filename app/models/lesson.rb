4a9f644df6259ee72a15ce22165ccbf56bfd086b
Carlos <carlos.emmanuel.236@gmail.com>
2022-05-20 20:22:34 -0300

    Adicionando novas validações no model de lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,8 +1,8 @@
 class Lesson < ApplicationRecord
   belongs_to :subject
-  has_one :rating
+  has_many :ratings
 
-  validates :title, presence: true, length: { minimum: 5, maximum: 20 }
+  validates :title, presence: true, length: { minimum: 5, maximum: 20 }, format: { with: /\A[a-zA-Z0-9\s]+\z/ }
   validates :description, presence: true, length: { minimum: 10 }
   validates :delivery_date, presence: true, date: { after: Proc.new { Time.now }, message: "A data deve ser posterior a hoje" }
   validates :bimester, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 4 }
