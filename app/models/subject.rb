624e49f26b9306c968dfc1407487fd42b73fe8d9
Carlos <carlos.emmanuel.236@gmail.com>
2022-05-20 01:21:03 -0300

    Adicionando model de subject

---
+++ app/models/subject.rb
@@ -0,0 +1,17 @@
+class Subject < ApplicationRecord
+  belongs_to :user
+  belongs_to :classroom
+  has_many :students, through: :classroom
+  has_many :lessons, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 50 }, format: { with: /\A[a-zA-Z0-9\s]+\z/, message: 'Somente letras e números são permitidas.' }
+  validates :year, presence: true, numericality: { only_integer: true }
+  validate :year_equal
+
+  private
+  def year_equal
+    unless year.equal?(self.classroom.year)
+      errors.add(:base, 'Ano letivo não corresponde ao da turma.')
+    end
+  end
+end
