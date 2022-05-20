32b5ab99e3e8aa6d475438fe6a77dc4a5848bb25
Carlos <carlos.emmanuel.236@gmail.com>
2022-05-20 19:30:15 -0300

    Adicionando testes do model user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Não deve salvar um usuário com nome que contém números " do
+    user = User.new nome: "Luis 123",
+                    email: "luis@gmail.com",
+                    password: "123456",
+                    password_confirmation: "123456"
+    assert_not user.save
+  end
+
+  test "Não deve salvar um usuário com senha menor que 6 caracteres" do
+    user = User.new nome: "Luis",
+                    email: "luis@gmail.com",
+                    password: "12345",
+                    password_confirmation: "12345"
+    assert_not user.save
+  end
 end
