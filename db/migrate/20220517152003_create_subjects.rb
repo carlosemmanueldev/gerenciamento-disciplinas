17fa9829e10f9f9d91d8201e035576f8e7932144
Carlos <carlos.emmanuel.236@gmail.com>
2022-05-20 00:58:06 -0300

    Adicionando migration de subject

---
+++ db/migrate/20220517152003_create_subjects.rb
@@ -0,0 +1,12 @@
+class CreateSubjects < ActiveRecord::Migration[7.0]
+  def change
+    create_table :subjects do |t|
+      t.string :name
+      t.integer :year
+      t.references :user, null: false, foreign_key: true
+      t.references :classroom, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
