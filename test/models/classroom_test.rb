a59370230d5cac38c44275dcaa4edfecfa53a5a0
Carlos <carlos.emmanuel.236@gmail.com>
2022-05-20 20:10:54 -0300

    Adicionando testes do model classroom

--- test/models/classroom_test.rb
+++ test/models/classroom_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class ClassroomTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Deve salvar uma turma cadastrada corretamente" do
+    user = User.new nome: "Usuario Teste",
+                    email: "usuarioteste@gmail.com",
+                    password: "123456",
+                    password_confirmation: "123456"
+    assert user.save
+
+    classroom = Classroom.new school_year: "8",
+                              letter: "A",
+                              year: "2022",
+                              user_id: user.id
+    assert classroom.save
+  end
+
+  test "Não deve salvar uma turma sem ano" do
+    user = User.new nome: "Usuario Teste",
+                    email: "usuarioteste@gmail.com",
+                    password: "123456",
+                    password_confirmation: "123456"
+    assert user.save
+
+    classroom = Classroom.new school_year: "8",
+                              letter: "A",
+                              year: nil,
+                              user_id: user.id
+    assert_not classroom.save
+  end
 end
