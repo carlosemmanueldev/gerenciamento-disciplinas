aeaf2eba0c823685a6e1cf741468f4a9c2318fc6
Carlos <carlos.emmanuel.236@gmail.com>
2022-05-20 20:22:49 -0300

    Adicionando novas validações no model de subject

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -4,10 +4,11 @@ class Subject < ApplicationRecord
   has_many :lessons, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }, format: { with: /\A[a-zA-Z0-9\s]+\z/, message: 'Somente letras e números são permitidas.' }
-  validates :year, presence: true, numericality: { only_integer: true }
+  validates :year, presence: true, length: { is: 4 }, numericality: { only_integer: true }
   validate :year_equal
 
   private
+
   def year_equal
     unless year.equal?(self.classroom.year)
       errors.add(:base, 'Ano letivo não corresponde ao da turma.')
