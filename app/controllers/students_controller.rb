4fe750e96e5ed511c5e2719531df9ae934d01779
Carlos <carlos.emmanuel.236@gmail.com>
2022-05-20 01:24:42 -0300

    Adicionando controller de student

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,72 @@
+class StudentsController < ApplicationController
+  before_action :set_student, only: %i[ show edit update destroy ]
+  before_action :logged_in_user
+
+  # GET /students or /students.json
+  def index
+    @students = Student.all
+  end
+
+  # GET /students/1 or /students/1.json
+  def show
+  end
+
+  # GET /students/new
+  def new
+    @student = Student.new
+  end
+
+  # GET /students/1/edit
+  def edit
+  end
+
+  # POST /students or /students.json
+  def create
+    @student = Student.new(student_params)
+
+    respond_to do |format|
+      if @student.save
+        format.html { redirect_to student_url(@student), notice: "O estudante foi cadastrado com sucesso!" }
+        format.json { render :show, status: :created, location: @student }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @student.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /students/1 or /students/1.json
+  def update
+    respond_to do |format|
+      if @student.update(student_params)
+        format.html { redirect_to student_url(@student), notice: "O estudante foi atualizado com sucesso!" }
+        format.json { render :show, status: :ok, location: @student }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @student.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /students/1 or /students/1.json
+  def destroy
+    @student.destroy
+
+    respond_to do |format|
+      format.html { redirect_to students_url, notice: "O estudante foi apagado com sucesso!" }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def student_params
+    params.require(:student).permit(:name, :document, :classroom_id)
+  end
+end
