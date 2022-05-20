539ad38bad4ce94b0b9b4198520430807611d327
Carlos <carlos.emmanuel.236@gmail.com>
2022-05-20 01:13:10 -0300

    Adicionando arquivo de schema do banco de dados

---
+++ db/schema.rb
@@ -0,0 +1,78 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_05_18_015611) do
+  create_table "classrooms", force: :cascade do |t|
+    t.string "school_year"
+    t.string "letter"
+    t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lessons", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.date "delivery_date"
+    t.integer "bimester"
+    t.integer "subject_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["subject_id"], name: "index_lessons_on_subject_id"
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.float "grade"
+    t.text "description"
+    t.integer "lesson_id", null: false
+    t.integer "student_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["lesson_id"], name: "index_ratings_on_lesson_id"
+    t.index ["student_id"], name: "index_ratings_on_student_id"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "document"
+    t.integer "classroom_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["classroom_id"], name: "index_students_on_classroom_id"
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.string "name"
+    t.integer "year"
+    t.integer "user_id", null: false
+    t.integer "classroom_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["classroom_id"], name: "index_subjects_on_classroom_id"
+    t.index ["user_id"], name: "index_subjects_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "lessons", "subjects"
+  add_foreign_key "ratings", "lessons"
+  add_foreign_key "ratings", "students"
+  add_foreign_key "students", "classrooms"
+  add_foreign_key "subjects", "classrooms"
+  add_foreign_key "subjects", "users"
+end
