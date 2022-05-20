41a78ea0c721e7852e454f67fb28a9b498e5d137
Carlos <carlos.emmanuel.236@gmail.com>
2022-05-20 20:11:26 -0300

    Adicionando testes do model student

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,42 @@
 require "test_helper"
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Deve salvar um estudante cadastrado corretamente" do
+    user = User.new nome: "Usuario Teste",
+                    email: "usuarioteste@gmail.com",
+                    password: "123456",
+                    password_confirmation: "123456"
+    assert user.save
+
+    classroom = Classroom.new school_year: "8",
+                              letter: "A",
+                              year: "2022",
+                              user_id: user.id
+    assert classroom.save
+
+    student = Student.new name: "Estudante Teste",
+                          document: "12345678900",
+                          classroom_id: classroom.id
+    assert student.save
+  end
+
+  test "Não deve salvar um estudante sem nome" do
+    user = User.new nome: "Usuario Teste",
+                    email: "usuarioteste@gmail.com",
+                    password: "123456",
+                    password_confirmation: "123456"
+    assert user.save
+
+    classroom = Classroom.new school_year: "8",
+                              letter: "A",
+                              year: "2022",
+                              user_id: user.id
+    assert classroom.save
+
+    student = Student.new name: nil,
+                          document: "12345678900",
+                          classroom_id: classroom.id
+    assert_not student.save
+  end
 end
