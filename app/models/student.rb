0d9247d37c7f08da81d19bce53fb6aee83aae7a5
Carlos <carlos.emmanuel.236@gmail.com>
2022-05-20 01:20:45 -0300

    Adicionando model de student

---
+++ app/models/student.rb
@@ -0,0 +1,8 @@
+class Student < ApplicationRecord
+  belongs_to :classroom
+  has_many :subjects
+  has_many :ratings
+
+  validates :name, presence: true, length: { minimum: 3 }, format: { with: /\A[a-zA-Z\s]+\z/, message: 'Somente letras são permitidas.' }
+  validates :document, presence: true, uniqueness: true, format: { with: /\A[0-9]{11}+\z/, :message => "O CPF deve conter 11 números" }
+end
