ee98e1de47dca951800ad5d015ee8ed843ea4cc6
Carlos <carlos.emmanuel.236@gmail.com>
2022-05-21 17:32:14 -0300

    Modificando as validações do model lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,8 +2,8 @@ class Lesson < ApplicationRecord
   belongs_to :subject
   has_many :ratings
 
-  validates :title, presence: true, length: { minimum: 5, maximum: 20 }, format: { with: /\A[a-zA-Z0-9\s]+\z/ }
-  validates :description, presence: true, length: { minimum: 10 }
-  validates :delivery_date, presence: true, date: { after: Proc.new { Time.now }, message: "A data deve ser posterior a hoje" }
+  validates :title, presence: true, length: { minimum: 5, maximum: 20, message: 'Tamanho do titulo de 5-20 caracteres' }, format: { with: /\A[a-zA-Z0-9\s]+\z/ }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :delivery_date, presence: true
   validates :bimester, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 4 }
 end
