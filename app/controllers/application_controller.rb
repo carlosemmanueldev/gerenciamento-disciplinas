017ee3738f48337ad2077f5ca4963b255ad27de7
Carlos <carlos.emmanuel.236@gmail.com>
2022-05-21 00:56:55 -0300

    Retirando linha de flash de mensagens na tela

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   def logged_in_user
     unless logged_in?
       store_location
-      flash[:danger] = "Por favor, faça login."
       redirect_to login_url
     end
   end
