b9cf4b2694ca0bc963aebae5161028fa5d907984
Carlos <carlos.emmanuel.236@gmail.com>
2022-05-22 18:28:04 -0300

    Adicionando novo metado no model rating

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -5,13 +5,18 @@ class Rating < ApplicationRecord
   validates :grade, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10, message: 'A nota deve estar entre 0 e 10' }
   validates :description, length: { maximum: 1000 }
   validate :student_is_not_already_rated
+  validate :student_is_in_lesson
 
   private
   def student_is_not_already_rated
     if Rating.where(lesson_id: lesson_id, student_id: student_id).exists?
-      errors.add(:student, 'O aluno já foi avaliado nesta aula')
+      errors.add(:student, 'O aluno já foi avaliado nesta atividade')
     end
   end
 
-
+  def student_is_in_lesson
+    if !lesson.students.include?(student)
+      errors.add(:student, 'O aluno não está na aula')
+    end
+  end
 end
